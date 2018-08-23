e9ce8cb58142a4022d592c227365720cc98241c7
Angela Vu <angela.vu@bellevuecollege.edu>
2018-08-23 16:49:04 -0700

    Fix Textarea to Allow Resizing to Fit Content

--- js/mubr-script.js
+++ js/mubr-script.js
@@ -1,8 +1,11 @@
 jQuery( document ).ready( function( $ ) {
-    
+
     //on textarea focus, select all
     $('#emails-textarea').focus( function() {
         this.select();
     });
+
+    //resize textarea to fit content
+    $("#emails-textarea").height( $("#emails-textarea")[0].scrollHeight );
 });
 
